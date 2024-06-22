4baf5aae23600a64ba4d2644b7e482f004c7add7
kyutae <ken2484@gmail.com>
2024-06-22 09:57:18 +0900

    init project

---
+++ src/main/java/kyutae/codetest/kcs/KcsApplication.java
@@ -0,0 +1,13 @@
+package kyutae.codetest.kcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KcsApplication.class, args);
+    }
+
+}
